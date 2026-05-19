e0cfe010956bb658aa5d1d741e0cefb5b8e1e32d
chrisgal585 <yukiromano123.ry@gmail.com>
2026-05-20 05:17:34 +0800

    Add files via upload

---
+++ scipt.js
@@ -0,0 +1,10 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+}, { threshold: 0.1 });
+
+document.querySelectorAll("section, .card, .dev-card, .categories a")
+.forEach(el => observer.observe(el));
